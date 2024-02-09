c55c3d15251d1511ade6cb3406d8e0d1aabad229
Shouryen <shouryen@protonmail.com>
2024-02-09 08:43:40 +0400

    Changed default admin to xien1 / admin@xien123

--- mock/user.ts
+++ mock/user.ts
@@ -36,7 +36,7 @@ export default {
           isLogin: false,
         },
         errorCode: '401',
-        errorMessage: '请先登录！',
+        errorMessage: 'Error',
         success: true,
       });
       return;
@@ -44,37 +44,17 @@ export default {
     res.send({
       success: true,
       data: {
-        name: 'Serati Ma',
+        name: 'Xien Admin',
         avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
         userid: '00000001',
         email: 'antdesign@alipay.com',
-        signature: '海纳百川，有容乃大',
-        title: '交互专家',
-        group: '蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED',
+        signature: 'Ni Hao',
+        title: 'Administrator',
+        group: 'Xien',
         tags: [
           {
             key: '0',
-            label: '很有想法的',
-          },
-          {
-            key: '1',
-            label: '专注设计',
-          },
-          {
-            key: '2',
-            label: '辣~',
-          },
-          {
-            key: '3',
-            label: '大长腿',
-          },
-          {
-            key: '4',
-            label: '川妹子',
-          },
-          {
-            key: '5',
-            label: '海纳百川',
+            label: 'Hello',
           },
         ],
         notifyCount: 12,
@@ -120,7 +100,7 @@ export default {
   'POST /api/login/account': async (req: Request, res: Response) => {
     const { password, username, type } = req.body;
     await waitTime(2000);
-    if (password === 'ant.design' && username === 'admin') {
+    if (password === 'admin@xien123' && username === 'xien1') {
       res.send({
         status: 'ok',
         type,
@@ -129,15 +109,15 @@ export default {
       access = 'admin';
       return;
     }
-    if (password === 'ant.design' && username === 'user') {
-      res.send({
-        status: 'ok',
-        type,
-        currentAuthority: 'user',
-      });
-      access = 'user';
-      return;
-    }
+    // if (password === 'ant.design' && username === 'user') {
+    //   res.send({
+    //     status: 'ok',
+    //     type,
+    //     currentAuthority: 'user',
+    //   });
+    //   access = 'user';
+    //   return;
+    // }
     if (type === 'mobile') {
       res.send({
         status: 'ok',
