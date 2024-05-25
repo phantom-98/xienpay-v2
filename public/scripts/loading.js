ce40317ff09dc12000efefd52da07aa8fabcb62d
Shouryen <shouryen@protonmail.com>
2024-05-26 00:20:13 +0400

    🌷 UI(sider): changed to dark theme in left side bar via token

--- public/scripts/loading.js
+++ public/scripts/loading.js
@@ -9,6 +9,7 @@
      <link rel="preconnect" href="https://fonts.googleapis.com">
      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,600;1,400&display=swap" rel="stylesheet">
+     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;1,400&display=swap" rel="stylesheet">
       <style>
         html,
         body,
@@ -16,7 +17,7 @@
           height: 100%;
           margin: 0;
           padding: 0;
-	  font-family: 'Noto Sans', sans-serif;
+	  font-family: 'Poppins', 'Noto Sans', sans-serif;
         }
         #root {
           background-repeat: no-repeat;
